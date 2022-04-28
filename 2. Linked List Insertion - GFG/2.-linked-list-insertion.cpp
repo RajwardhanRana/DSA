2c4077e0209c14720a5125ad10b09fafbff2dd30
Rajwardhan Rana <83343791+RajwardhanRana@users.noreply.github.com>
2022-04-28 19:27:11 +0530

    Added solution - LeetHub

--- 2. Linked List Insertion - GFG/2.-linked-list-insertion.cpp
+++ 2. Linked List Insertion - GFG/2.-linked-list-insertion.cpp
@@ -36,45 +36,27 @@ struct Node {
 
 class Solution{
   public:
-    // //Function to insert a node at the beginning of the linked list.
-    // Node *insertAtBegining(Node *head, int x) {
-    //   Node *t=new Node(x);
-    //   t->next=head;
-    //   return t;
-    // }
+    //Function to insert a node at the beginning of the linked list.
+    Node *insertAtBegining(Node *head, int x) {
+      Node *t=new Node(x);
+      t->next=head;
+      return t;
+    }
     
     
-    // //Function to insert a node at the end of the linked list.
-    // Node *insertAtEnd(Node *head, int x)  {
-    //   Node*c=head;
-    //     Node *t = new Node(x);
-    //   while(c->next!= NULL)
-    //   c=c->next;
-    //   c->next=t;
-    //   return head;
+    //Function to insert a node at the end of the linked list.
+    Node *insertAtEnd(Node *head, int x)  {
+        Node *t = new Node(x);
+         if(head==NULL)
+          return t;
+       Node*c=head;
+        
+       while(c->next!= NULL)
+       c=c->next;
+       c->next=t;
+       return head;
        
-    // }
-      Node *insertAtBegining(Node *head, int x) {
-      // Your code here
-      Node *temp=new Node(x);
-      temp->next = head;
-      return temp;
-   }
-   
-   //Function to insert a node at the end of the linked list.
-   Node *insertAtEnd(Node *head, int x)  {
-      // Your code here
-      Node *temp = new Node(x);
-      if(head==NULL)
-      return temp;
-      
-      Node *curr=head;
-      while(curr->next!=NULL)
-      curr=curr->next;
-      
-      curr->next=temp;
-      return head;
-   }
+    }
 };
 
 
